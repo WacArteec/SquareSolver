ebdb923c161904e6179a8f0f1cfc9f6003bf5690
WacArteec <art.kaznad@gmail.com>
2024-08-23 21:30:21 +0300

    21-30-23-08-24

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@
 #define BLACK   "\033[30m"      /* Black */
 const float COMPARECONST = 1e-12;
 enum constantnumbers {Zero, One, Two, Infinity};
-
+/**
+*/
 struct Parameters
 
 {
@@ -37,8 +38,8 @@ void PrintSolves(const Solution sol);
 void UserInputAndCheck(float coef[3]);
 void GetcharReset();
 void SquareEquation(const float a, const float b, const float c, Solution * const sol);
-int Min(const float a, const float b);
-int Max(const float a, const float b);
+float Min(const float a, const float b);
+float Max(const float a, const float b);
 float FabsZero(const float a);
 
 int main()
@@ -100,8 +101,9 @@ void UserInputAndCheck(float coef[3])
 {
     for(int i = 0; i < 3; i++)
     {
+        char a;
         printf("Enter Parameter %c:\n",'a'+i);
-        if(scanf("%g", &coef[i]) != 1)
+        if(scanf("%g%c", &coef[i], &a) != 2)
         {
             GetcharReset();
             printf("You can enter only numbers\n");
@@ -112,7 +114,7 @@ void UserInputAndCheck(float coef[3])
             printf("You can enter only numbers\n");
             i--;
         }
-        else if(!EqualSymbol('\n'))
+        else if(a!='\n' && !EqualSymbol('\n'))
         {
             printf("You can enter only numbers\n");
             i--;
@@ -214,13 +216,13 @@ float FabsZero(const float a)
     else return a;
 }
 
-int Min(const float a, const float b)
+float Min(const float a, const float b)
 {
     if(a < b) return a;
     else return b;
 }
 
-int Max(const float a, const float b)
+float Max(const float a, const float b)
 {
     if(a > b) return a;
     else return b;
@@ -233,10 +235,10 @@ void PrintSolves(const Solution sol)
         case Zero:  printf("no solves\n");
                     break;
 
-        case One:   printf("x= %g\n", sol.root1);
+        case One:   printf("x= %.2f\n", sol.root1);
                     break;
 
-        case Two:   printf("x1 = %g x2 = %g\n", sol.root1, sol.root2);
+        case Two:   printf("x1 = %.2f x2 = %.2f\n", sol.root1, sol.root2);
                     break;
 
         case Infinity:  printf("infinity count of solves\n");
